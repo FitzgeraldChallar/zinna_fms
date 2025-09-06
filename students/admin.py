cf0f5c77f1f000da2f6c6d3dfb78edd1bc5f8e31
FitzgeraldChallar <challararise@gmail.com>
2025-09-06 17:06:51 +0000

    ordered students names alphabetically

--- students/admin.py
+++ students/admin.py
@@ -11,3 +11,6 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ('student_id', 'full_name', 'grade', 'total_fees', 'total_paid', 'balance')
     list_filter = ('grade',)  # Enables filtering by grade in sidebar
     search_fields = ('full_name', 'student_id')
+
+    # Order the list alphabetically by full_name
+    ordering = ('full_name',)
