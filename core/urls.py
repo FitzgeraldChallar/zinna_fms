1e5643bfbdaa36aff29adcb6173bc4bbd2ed890d
FitzgeraldChallar <challararise@gmail.com>
2025-09-09 19:31:40 +0000

    new change

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include  # Include is useful if you have app-level URLs
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('students/', include('students.urls')),
-    # You can also add other app routes like:
-    # path('students/', include('students.urls')),
     # path('transactions/', include('transactions.urls')),
 ]
 
+# 👇 Always serve media (DEBUG or not)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
