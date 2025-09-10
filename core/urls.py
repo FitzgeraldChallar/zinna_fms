3b86552528dd72060d4c02d6f704362908bf541a
FitzgeraldChallar <challararise@gmail.com>
2025-09-10 11:23:12 +0000

    new change

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +12,9 @@ urlpatterns = [
     #path('transactions/', include('transactions.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Production-ready serving of media (only works with Render's web service)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
